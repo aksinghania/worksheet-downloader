e7e066a667b8980e7fe20d59c5350fdd8a9faee8
aksinghania <akshtsinghnia@gmail.com>
2024-08-03 20:38:35 +0530

    added code

--- popup.js
+++ popup.js
@@ -1,41 +1,45 @@
-document.getElementById("removeButton").addEventListener("click", () => {
-  const selector = document.getElementById("selector").value.trim();
-
-  if (selector) {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      chrome.scripting.executeScript({
-        target: { tabId: tabs[0].id },
-        func: removeElements,
-        args: [selector],
-      });
+document.getElementById("modifyButton").addEventListener("click", () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.scripting.executeScript({
+      target: { tabId: tabs[0].id },
+      func: modifyPage,
     });
-  }
+  });
 });
 
-document.getElementById("marginButton").addEventListener("click", () => {
-  const selector = document.getElementById("marginSelector").value.trim();
+function modifyPage() {
+  // Classes to remove
+  const classesToRemove = [
+    "question-actions",
+    "css-4yer0f",
+    "css-okp1i3",
+    "MuiAppBar-positionFixed",
+    "css-1vz94wv",
+    "css-4ejgit",
+    "css-1pk3wz",
+    "css-vubbuv",
+    "css-7d979a",
+    "MuiFab-circular",
+    "css-g1i8p9",
+    "css-1s7zn6p",
+  ];
+  classesToRemove.forEach((className) => {
+    document
+      .querySelectorAll(`.${className}`)
+      .forEach((element) => element.remove());
+  });
 
-  if (selector) {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      chrome.scripting.executeScript({
-        target: { tabId: tabs[0].id },
-        func: setMarginTopToZero,
-        args: [selector],
-      });
+  // Properties to modify
+  const propertiesToModify = [
+    { className: "css-1vnzknk", property: "marginTop", value: "0px" },
+    { className: "css-923ro0", property: "padding", value: "0px" },
+    { className: "css-k2lcna", property: "marginBottom", value: "0px" },
+    { className: "css-k2lcna", property: "marginTop", value: "-50px" },
+  ];
+
+  propertiesToModify.forEach(({ className, property, value }) => {
+    document.querySelectorAll(`.${className}`).forEach((element) => {
+      element.style[property] = value;
     });
-  }
-});
-
-function removeElements(selector) {
-  const elementsById = document.querySelectorAll(`#${selector}`);
-  const elementsByClass = document.querySelectorAll(`.${selector}`);
-
-  elementsById.forEach((element) => element.remove());
-  elementsByClass.forEach((element) => element.remove());
-}
-
-function setMarginTopToZero(selector) {
-  const elementsByClass = document.querySelectorAll(`.${selector}`);
-  elementsByClass.forEach((element) => (element.style.marginTop = `-50px`));
-  elementsByClass.forEach((element) => (element.style.marginBottom = `-50px`));
+  });
 }
